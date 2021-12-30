f1d5674214aeee1f73042f8cf352c2334ea8de84
Abdul-Majid Nuhu <nuhuabdulmajid58@gmail.com>
2021-12-30 11:47:52 +0000

    Added page pagination and view class

--- blog/blog_app/urls.py
+++ blog/blog_app/urls.py
@@ -17,7 +17,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail, name='post_detail'),
 ]
