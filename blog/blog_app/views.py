a692f5318db733a51bedba9ed0585642fcaa5d42
Abdul-Majid Nuhu <nuhuabdulmajid58@gmail.com>
2022-01-06 13:10:23 +0000

    added a forms view

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import EmailPostForm
 # from django.core.paginator import Paginator, EmptyPage, \
 #     PageNotAnInteger
 from django.views.generic import ListView
@@ -45,3 +46,19 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog_app/post/detail.html',
                   {'post': post})
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == 'POST':
+        # Forms was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form field is submitted
+            cd = form.cleaned_data
+            # Send email
+        else:
+            form = EmailPostForm()
+        return render(request, 'blog_app/post/share.html', {'post': post,
+                                                            'form': form})
