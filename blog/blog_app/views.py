14b945c7fe3f8eb1aff745df765aa7d8f6b10560
Abdul-Majid Nuhu <nuhuabdulmajid58@gmail.com>
2022-01-10 12:40:31 +0000

    created a post share view

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 # from django.core.paginator import Paginator, EmptyPage, \
 #     PageNotAnInteger
 from django.views.generic import ListView
@@ -51,14 +52,24 @@ def post_detail(request, year, month, day, post):
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
     if request.method == 'POST':
         # Forms was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Form field is submitted
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url)
+            subject = f"{cd['name']} recommends you to  read " \
+                f"{post.title}"
+            message = f"Read {post.title} at {post_url} \n \n" \
+                f"{cd['name']} \'s comments: {cd['comments']}"
             # Send email
-        else:
-            form = EmailPostForm()
-        return render(request, 'blog_app/post/share.html', {'post': post,
-                                                            'form': form})
+            send_mail(subject, message,
+                      'nuhuabdulmajid58@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog_app/post/share.html', {'post': post,
+                                                        'form': form,
+                                                        'sent': sent})
